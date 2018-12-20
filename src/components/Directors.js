3f620a34a54174c2d8da63e7b4275fc3746b1d8b
said shah <saidmysr20@aol.com>
2018-12-19 20:08:09 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,10 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      <div>{directors[0].name} {directors[0].movies.join(",")}</div>
+      <div>{directors[1].name} {directors[1].movies.join(",")}</div>
+      <div>{directors[2].name} {directors[2].movies.join(",")}</div>
     </div>
   );
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -10,10 +10,17 @@ import Directors from '../components/Directors';
 import Movies from '../components/Movies';
 
 
+
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+        <div>
+          <NavBar/>
+          <Route path="/" component={Home}/>
+          <Route path="/actors" component={Actors}/>
+          <Route path="/directors" component={Directors}/>
+          <Route path="/movies" component={Movies} allMovies={props}/>
+        </div>
     </Router>
   );
 };
