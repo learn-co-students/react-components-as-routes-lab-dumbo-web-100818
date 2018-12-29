4829e0b296f72ba9ba716f2bda08b8540c99c6f2
hmalik88 <hbmalik88@gmail.com>
2018-12-28 19:17:04 -0500

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Home from '../components/Home';
 import Actors from '../components/Actors';
@@ -13,9 +10,14 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
-    </Router>
-  );
+      <React.Fragment>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/actors" component={Actors} />
+        <Route exact path="/directors" component={Directors} />
+        <Route exact path="/movies" component={Movies} />
+      </React.Fragment>
+    </Router>)
 };
 
 export default App
